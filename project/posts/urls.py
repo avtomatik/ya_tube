5a2c3751afa5f22b4c9e411fd2dbf17891f41b09
Alexander <kkruscherr@gmail.com>
2024-07-07 20:02:18 +0300

    Up to Sprint 13; Lesson 06; Problem 2/2

--- project/posts/urls.py
+++ project/posts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/v1/posts/', views.api_posts, name='api_posts'),
+    path('api/v1/posts/', views.api_posts),
+    path('api/v1/posts/<int:pk>/', views.api_posts_detail)
 ]
--- project/posts/views.py
+++ project/posts/views.py
@@ -1,3 +1,5 @@
+
+from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -12,12 +14,26 @@ def api_posts(request):
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(
-                data=serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            data=serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     posts = Post.objects.all()
     serializer = PostSerializer(posts, many=True)
-    return Response(data=serializer.data)
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def api_posts_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == 'PUT' or request.method == 'PATCH':
+        serializer = PostSerializer(post, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        post = Post.objects.get(pk=pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    post = Post.objects.get(pk=pk)
+    serializer = PostSerializer(post)
+    return JsonResponse(data=serializer.data)
